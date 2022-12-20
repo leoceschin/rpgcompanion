f4b21ff4544739a7af7f82cf7dd50f36e2e50318
leona <leonardofcoceschin@gmail.com>
2022-12-20 11:06:40 -0300

    client realizando as funcoes basicas

--- pyclient/bot.py
+++ pyclient/bot.py
@@ -3,9 +3,10 @@ import os
 import discord
 from discord.ext import commands
 import requests
+import json
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv('./config/.env')
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 BASE_URL='http://localhost:8081/'
@@ -36,5 +37,48 @@ async def gameinit(ctx):
     r = requests.post(BASE_URL+"game-init/"+str(id))
     if (r.status_code == 200):
         await ctx.send("Jogo iniciado")
-        
+
+@bot.command()
+async def addplayer(ctx, name: str, life: int):
+    payload = {
+        "id": ctx.message.author.id,
+        "name": name,
+        "chatId": ctx.message.channel.id,
+        "lifePoints": life
+    }
+    r = requests.post(BASE_URL+"add-player", json=payload)
+    await ctx.send("personagem adicionado")
+
+@bot.command()
+async def adddamage(ctx, name: str, diceRaw: str, attr=0):
+    dice = diceRaw.split('d')
+    sides = int(dice[1])
+    quantity = int(dice[0])
+    
+    payload = {
+        "chatId": ctx.message.channel.id,
+        "name": name,
+        "sides": sides,
+        "quantity": quantity,
+        "attributes": attr
+    }
+    r = requests.put(BASE_URL+"add-damage", json=payload)
+    await ctx.send(r.content)
+
+@bot.command()
+async def roll(ctx, diceRaw: str, attr=0, name="",):
+    dice = diceRaw.split('d')
+    sides = int(dice[1])
+    quantity = int(dice[0])
+    
+    payload = {
+        "chatId": ctx.message.channel.id,
+        "name": name,
+        "sides": sides,
+        "quantity": quantity,
+        "attributes": attr
+    }
+    r = requests.get(BASE_URL+"roll", json=payload)
+    await ctx.send(r.content)
+
 bot.run(TOKEN)
